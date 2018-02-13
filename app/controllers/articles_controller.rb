65f1c7064e150fc70d1920cde57a4d1b71168ffb
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-02-13 16:12:47 +0100

    Exo 2

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def article_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 10 fake aricles (title, content)
+require 'faker'
+
+10.times do |article|
+  # Random Book Title
+  Article.create(title: Faker::Book.title, content: Faker::Lorem.paragraph)
+end
